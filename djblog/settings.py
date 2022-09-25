58d37a5cbc6886cad18f7263b5c9162f8cc30346
Jesse <admin@admin.com>
2022-09-25 22:16:43 +0800

    Changed settings.

--- djblog/settings.py
+++ djblog/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = "static_root"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
